cfc148329f37d1ffac234dd09edf6277ef54744c
Bo Amusa <boamusa@gmail.com>
2025-06-08 07:35:08 -0400

    Updating local proxy

--- stock-app-vite/vite.config.ts
+++ stock-app-vite/vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:7071',
-    }
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: isDev
+        ? {
+            '/api': {
+              target: 'http://127.0.0.1:7071',
+              changeOrigin: true,
+            },
+          }
+        : undefined,
+    },
   }
 })
 
