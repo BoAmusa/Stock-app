816029eb77131ce6acb9c31d04ce1601511c184d
Bo Amusa <boamusa@gmail.com>
2025-06-07 21:06:14 -0400

    Add routing

--- stock-app-vite/src/App.tsx
+++ stock-app-vite/src/App.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import "./App.css";
 import Landing from "./components/landing/Landing";
+import { Routes, Route, Navigate } from "react-router-dom";
+import StockBase from "./components/stock-base/StockBase";
 
 const App: React.FC = () => {
-  return <Landing />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/landing" replace />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/stockbase" element={<StockBase />} />
+    </Routes>
+  );
 };
 
 export default App;
